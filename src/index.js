183bb4c95d3083bc4d349b15271b6f3ffb9fb4fd
kiyoung <popping2606@gmail.com>
2020-11-05 07:28:04 +0900

    npm i redux-thunk

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import rootReducer from "./modules/index"
 import { Provider } from "react-redux"
 import { composeWithDevTools } from "redux-devtools-extension" // 리덕스 개발자 도구
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger))) // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))) // 스토어를 만듭니다.
 
 ReactDOM.render(
   <React.StrictMode>
